43470fc1a9f23dde01b7275ff4901e991d0deb63
sassyCorbeau <sassycorbeau@gmail.com>
2020-02-05 10:23:34 +0100

    Script EnemyFollow créé, script GameManager créé

---
+++ GladiArena/Assets/Assets/Script/EnemyFollow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+
+    public float speed = 5f;
+    //Changer sur l'inspector si la vitesse ne va pas
+
+    public Transform Player;
+    //Remplir sur l'inspector avec le Player
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 displacement = Player.position - transform.position;
+        displacement = displacement.normalized;
+        if (Vector2.Distance(Player.position, transform.position) > 1.0f)
+        {
+            transform.position += (displacement * speed * Time.deltaTime);
+        }
+
+        else
+        {
+            /*
+             variable de vie -1
+             destroy all enemies
+             variable de power-up = 0
+
+            à remplir après dans le script du GameManager un if(variable de vie = 0) --> ouvrir scene de game over
+            */
+        }
+
+    }
+
+}
