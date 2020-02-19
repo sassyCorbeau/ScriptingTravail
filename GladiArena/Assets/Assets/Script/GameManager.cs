4472b4cffe524e9f3b119645d358ee561fb09164
Appirial <ludivine.chatin@gmail.com>
2020-02-19 10:59:08 +0100

    power up collect validé

--- GladiArena/Assets/Assets/Script/GameManager.cs
+++ GladiArena/Assets/Assets/Script/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int PowerUP ;
+    public static int PowerUP ;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +14,9 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (PowerUP == 1)
+        {
+            Debug.Log("Power up is nik twa!");
+        }
     }
 }
