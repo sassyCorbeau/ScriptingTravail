75a1497980f35a634c6151326c8a864fb4f05358
EtCtulu <bertrandemilien17@gmail.com>
2020-02-19 11:42:15 +0100

    Création d'un score fonctionel a la foi dans le menu de game over et en jeu, les ennemis donnent 50 2fois donc 100. Vous pouvez vous aussi ajouter une augmentation de score sur vôtre script avec. ScoreCounter.currentScore = ScoreCounter.currentScore + 50f;

---
+++ GladiArena/Assets/Assets/Script/ScoreCounter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+
+
+    public static float currentScore = 0;
+    public Text scoreText;
+    public Text scoreLoser;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentScore = 0;
+
+    }
+
+    private void Update()
+    {
+        if (currentScore == 100)
+        {
+            Debug.Log("Le score as augmenté");
+        }
+        scoreText.text = "Score : " + currentScore;
+        scoreLoser.text = "Score : " + currentScore;
+    }
+
+  /*  private void HandleScore()
+    {
+
+        scoreText.text = "Score : " + currentScore;
+    } */
+
+
+    
+
+}
