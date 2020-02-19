747e5fa569e33d1312fe80c35d95b81f1cd82d93
Appirial <ludivine.chatin@gmail.com>
2020-02-19 11:01:28 +0100

    no message

--- GladiArena/Assets/Assets/Script/GameManager.cs
+++ GladiArena/Assets/Assets/Script/GameManager.cs
@@ -14,9 +14,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (PowerUP == 1)
-        {
-            Debug.Log("Power up is nik twa!");
-        }
+        
     }
 }
