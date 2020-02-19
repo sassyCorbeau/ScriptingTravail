6283b737eab8765e6b71cf5ded0437bc74be1a3a
sassyCorbeau <sassycorbeau@gmail.com>
2020-02-19 10:13:26 +0100

    Début de script de spawn ennemi

---
+++ GladiArena/Assets/Assets/Script/MonsterSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    //Script à mettre sur un gameObject empty
+
+    public Transform[] spawnPoints;
+    //remplir avec le nombre de spawns (donc 4 normalement) et avec les gameObject de spawn
+    public GameObject[] monsters;
+    //remplir avec le nombre d'ennemis différents et les prefabs des ennemis
+    int randomSpawnPoint;
+    int randomMonster;
+    public static bool spawnAllowed;
+
+    void Start()
+    {
+        spawnAllowed = true;
+        InvokeRepeating("SpawnAMonster", 0f, 1f);
+    }
+  
+    void Update()
+    {
+        
+    }
+
+    void SpawnAMonster()
+    {
+        if (spawnAllowed)
+        {
+            randomSpawnPoint = Random.Range(0, spawnPoints.Length);
+            randomMonster = Random.Range(0, monsters.Length);
+            Instantiate(monsters[randomMonster], spawnPoints[randomSpawnPoint].position, Quaternion.identity);
+        }
+    }
+}
