cbf86c5c6eb7a9f5191f0ad384190320c6c82796
EtCtulu <bertrandemilien17@gmail.com>
2020-03-11 09:49:13 +0100

    Commenté le script player shoot texte, et mon script de mulihit marche !!!!

--- GladiArena/Assets/Assets/Script/Player/PlayerShootTexte.cs
+++ GladiArena/Assets/Assets/Script/Player/PlayerShootTexte.cs
@@ -2,18 +2,13 @@
 using System.Collections;
 
 [System.Serializable]
-public class Boundary
-{
-    public float xMin, xMax, zMin, zMax;
-}
+
 
 public class PlayerShootTexte : MonoBehaviour
 {
-    public float speed;
-    public float tilt;
-    public Boundary boundary;
 
-    public GameObject shotUp;
+
+    public GameObject shotUp; // Les différents tirs
     public GameObject shotDown;
     public GameObject shotLeft;
     public GameObject shotRight;
@@ -21,12 +16,47 @@ public class PlayerShootTexte : MonoBehaviour
     public GameObject shotUpLeft;
     public GameObject shotDownRight;
     public GameObject shotDownLeft;
-    public static float fireRate = 0.5f;
+    public static float fireRate = 0.5f; // La cadence 
 
-    private float nextFire;
+    private float nextFire; // La variable pour la cadence
 
     void Update()
     {
+        // MultiHit
+
+        if (GameManager.multiHit == true)
+        {
+
+            Debug.Log("Multihit = True");
+
+            GameManager.timerPower = GameManager.timerPower + Time.deltaTime;
+
+            Debug.Log("Multihit changée");
+            if (Input.GetKey(KeyCode.RightArrow) && Time.time > nextFire || (Input.GetKey(KeyCode.LeftArrow) && Time.time > nextFire || (Input.GetKey(KeyCode.DownArrow) && Time.time > nextFire || (Input.GetKey(KeyCode.UpArrow) && Time.time > nextFire))))
+            {
+                nextFire = Time.time + fireRate;
+                Instantiate(shotUp, transform.transform);
+                Instantiate(shotRight, transform.transform);
+                Instantiate(shotLeft, transform.transform);
+                Instantiate(shotDown, transform.transform);
+                Instantiate(shotDownLeft, transform.transform);
+                Instantiate(shotDownRight, transform.transform);
+                Instantiate(shotUpLeft, transform.transform);
+                Instantiate(shotUpRight, transform.transform);
+                
+            }
+            if (GameManager.timerPower > 10)
+            {
+                GameManager.multiHit = false;
+                Debug.Log("Multihit End");
+                PlayerShootTexte.fireRate = 0.5f;
+                GameManager.timerPower = 0;
+                return;
+            }
+        }
+
+        // Shot
+
         if (GameManager.multiHit == false)
         {
             if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow) && Time.time > nextFire)
@@ -79,37 +109,7 @@ public class PlayerShootTexte : MonoBehaviour
                 Instantiate(shotRight, transform.transform);
             }
 
-            // MultiHit
-
-            if (GameManager.multiHit == true)
-            {
-
-                Debug.Log("Multihit = True");
-
-                GameManager.timerPower = GameManager.timerPower + Time.deltaTime;
-
-                Debug.Log("Multihit changée");
-                if (Input.GetKey(KeyCode.RightArrow) || (Input.GetKey(KeyCode.LeftArrow) || (Input.GetKey(KeyCode.DownArrow) || (Input.GetKey(KeyCode.UpArrow)))))
-                {
-                    nextFire = Time.time + fireRate;
-                    Instantiate(shotUp, transform.transform);
-                    Instantiate(shotRight, transform.transform);
-                    Instantiate(shotLeft, transform.transform);
-                    Instantiate(shotDown, transform.transform);
-                    Instantiate(shotDownLeft, transform.transform);
-                    Instantiate(shotDownRight, transform.transform);
-                    Instantiate(shotUpLeft, transform.transform);
-                    Instantiate(shotUpRight, transform.transform);
-                }
-                if (GameManager.timerPower > 10)
-                {
-                    GameManager.multiHit = false;
-                    Debug.Log("Multihit End");
-                    PlayerShootTexte.fireRate = 0.5f;
-                    GameManager.timerPower = 0;
-                    return;
-                }
-            }
+            
         }
 
         
