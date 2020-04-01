537ee688eca4a56c9f5506276f048e124a7cdc30
Appirial <ludivine.chatin@gmail.com>
2020-04-01 11:33:13 +0200

    tagger

--- GladiArena/Assets/Assets/Script/Sang.cs
+++ GladiArena/Assets/Assets/Script/Sang.cs
@@ -28,25 +28,12 @@ public class Sang : MonoBehaviour
         }
         if (sangLaiiser == 1)
         {
-            
+
             Debug.Log("Rien");
             return;
         }
-
-
-<<<<<<< HEAD
-=======
-    public void OnDestroy()
-    {
-
-        Instantiate(tacheDeSang, transform.position, transform.rotation);
-<<<<<<< HEAD
-
-=======
->>>>>>> 71d8660e45d5eeeee7fb8293a69b4fa26aa939c5
->>>>>>> 0b1d92fe36794e15b74bae28b48c335ca9abc617
->>>>>>> d1ed988c3ab1cb8814fddcfd959bed74c0158007
     }
+    
 }
 
 
