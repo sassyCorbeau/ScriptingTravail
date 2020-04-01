7c5d553eac30b259a37159bb0718de2b7928f204
Appirial <ludivine.chatin@gmail.com>
2020-04-01 10:30:24 +0200

    bloup

---
+++ GladiArena/Assets/Assets/Script/Sang.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sang : MonoBehaviour
+{
+    public GameObject tacheDeSang;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnDestroy()
+    {
+        Instantiate(tacheDesang, transform.position, transform.rotation);
+    }
+}
+
+
