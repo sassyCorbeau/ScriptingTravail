86181e09e0527e9cac33a1b2fd1d91c206b4d8bf
sassyCorbeau <sassycorbeau@gmail.com>
2020-04-01 11:28:00 +0200

    géparéussi

--- GladiArena/Assets/Assets/Script/Sang.cs
+++ GladiArena/Assets/Assets/Script/Sang.cs
@@ -18,34 +18,23 @@ public class Sang : MonoBehaviour
         {
             Debug.Log("Trainner 1");
             Instantiate(tacheDeSang, transform.position, transform.rotation);
+
             return;
         }
         if (sangLaiiser == 1)
         {
             Instantiate(tacheDeSang2, transform.position, transform.rotation);
             Debug.Log("Trainner 2");
+
             return;
         }
-        if (sangLaiiser == 1)
+        if (sangLaiiser == 2)
         {
-            
+
             Debug.Log("Rien");
             return;
         }
 
-
-<<<<<<< HEAD
-=======
-    public void OnDestroy()
-    {
-
-        Instantiate(tacheDeSang, transform.position, transform.rotation);
-<<<<<<< HEAD
-
-=======
->>>>>>> 71d8660e45d5eeeee7fb8293a69b4fa26aa939c5
->>>>>>> 0b1d92fe36794e15b74bae28b48c335ca9abc617
->>>>>>> d1ed988c3ab1cb8814fddcfd959bed74c0158007
     }
 }
 
